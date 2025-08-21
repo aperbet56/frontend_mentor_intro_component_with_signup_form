6345a932e88e982610248b1c1ee87ee7dd53d0de
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-08-21 16:17:13 +0200

    Ajout des regex

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ const firstNameErrorMessage = document.querySelector("#firstName__error");
 const lastNameErrorMessage = document.querySelector("#lastName__error");
 const emailErrorMessage = document.querySelector("#email__error");
 const passwordErrorMessage = document.querySelector("#password__error");
+
+// Regex
+
+const regexName = /^[A-Z][A-Za-z\é\è\ê\ô\-]+$/;
+const regexEmail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+const regexPassword =
+  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
